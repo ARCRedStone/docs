9bfa35979fb648da7f6a1e95901b98ca730d43fd
HollowDobt <2901448780@qq.com>
2026-01-24 10:54:48 +0800

    docs: update from 8124f1f

--- namespaces_dup.js
+++ namespaces_dup.js
@@ -3,6 +3,7 @@ var namespaces_dup =
     [ "timerTools", "namespacetimer_tools.html", [
       [ "get_autoReloadRegister", "namespacetimer_tools.html#a31bc141bfc92e5f27a1fb08b11f935a0", null ],
       [ "get_overFlowFrequency", "namespacetimer_tools.html#adde500976c0501f7c729c0b373904983", null ],
+      [ "get_overFlowTime_ms", "namespacetimer_tools.html#a57c42a2dec66da47bc5c3ebd8a0cb279", null ],
       [ "get_overFlowTime_us", "namespacetimer_tools.html#a67c82247abfea2a2601e669654d32441", null ]
     ] ]
 ];
--- timer__tools_8h.js
+++ timer__tools_8h.js
@@ -3,5 +3,6 @@ var timer__tools_8h =
     [ "__HAL_TIM_GET_PRESCALER", "timer__tools_8h.html#a3d236c1765201d49ee0f728c6e566cfa", null ],
     [ "timerTools::get_autoReloadRegister", "namespacetimer_tools.html#a31bc141bfc92e5f27a1fb08b11f935a0", null ],
     [ "timerTools::get_overFlowFrequency", "namespacetimer_tools.html#adde500976c0501f7c729c0b373904983", null ],
+    [ "timerTools::get_overFlowTime_ms", "namespacetimer_tools.html#a57c42a2dec66da47bc5c3ebd8a0cb279", null ],
     [ "timerTools::get_overFlowTime_us", "namespacetimer_tools.html#a67c82247abfea2a2601e669654d32441", null ]
 ];
